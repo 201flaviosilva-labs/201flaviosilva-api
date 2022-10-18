87034661ab70603e1fc3d470e9be4b8cee71dd14
201flaviosilva <flavioandre.2001.9.7@gmail.com>
2022-10-18 10:00:50 +0000

    fix viewer count

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const cookieParser = require("cookie-parser");
 const logger = require("morgan");
 
 const indexRouter = require("./routes/index");
-const viewerCountRouter = require("./routes/viewer-count");
+const viewerCounterRouter = require("./routes/viewer-counter");
 
 const app = express();
 const port = process.env.PORT || 5001;
@@ -21,7 +21,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
-app.use("/viewer-count", viewerCountRouter);
+app.use("/viewer-counter", viewerCounterRouter);
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
---
+++ routes/viewer-counter.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const router = express.Router();
+const { v4: uuidv4 } = require('uuid');
+
+const items = []; // {id: "beep", value: 10}
+
+function findId(id) {
+  const item = items.find(i => i.id === id);
+
+  if (!item) {
+    const newItem = { id: uuidv4(), value: 0 };
+    items.push(newItem);
+    return newItem;
+  }
+
+  return item;
+}
+
+router.get("/", (req, res, next) => {
+  const item = findId(req.query.id);
+  item.value++;
+  res.json(items);
+});
+
+module.exports = router;
